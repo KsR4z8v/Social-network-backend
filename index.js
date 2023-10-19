707fe3dee6f952644d657103b1a65ec325945600
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-10-19 13:17:49 -0500

    initial project

---
+++ index.js
@@ -0,0 +1,8 @@
+import app from "./src/app.js";
+import dotenv from "dotenv"
+dotenv.config()
+const PORT = process.env.PORT ?? 8000
+
+app.listen(PORT, () => [
+    console.log(`Server listen on port: ${PORT}`)
+])
