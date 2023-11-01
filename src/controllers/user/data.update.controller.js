f26abe491ff78057da93c9606ded0f01398622cc
Oscar Rivera <oscar.pardo@correounivalle.edu.co>
2023-10-31 20:19:00 -0500

    Implements controllers to update the user personal info.

---
+++ src/controllers/user/data.update.controller.js
@@ -0,0 +1,27 @@
+import services from '../../database/models/index.js'
+import responseTemplate from '../../handlersResponses/responseTemplates.js';
+
+const { internalError, userNotFound} = responseTemplate
+
+const dataUpdate = async (req, resp) => {
+try {
+        const{id_user,fullname,username,phone_number,email,date_born} = req.body
+
+
+
+        const resp_db = await services.usersService.dataUpdate(id_user, fullname, username, phone_number, email, date_born)
+
+        if(!resp_db){
+            return resp.status(404).json(userNotFound())
+        }
+
+    return resp.status(200).json({ message: 'OK'})
+} catch (error) {
+    console.log(error);
+    resp.status(500).json(internalError())
+    
+}
+
+}
+
+export default dataUpdate
--- src/database/models/userModel.js
+++ src/database/models/userModel.js
@@ -11,5 +11,13 @@ export default (pool) => ({
         const resp_db = await client.query(`UPDATE users SET url_avatar=$1 WHERE id_user=$2 RETURNING id_user`, [url_avatar, id_user])
         client.release()
         return resp_db.rows[0]
+    },
+
+    dataUpdate: async (id_user,fullname,username,phone_number,email,date_born) => {
+        const client = await pool.connect()
+        const resp_db = await client.query(`UPDATE users SET fullname=$1 , username=$2 , phone_number=$3 , email=$4, date_born=$5 WHERE id_user=$6 RETURNING id_user`, [fullname,username,phone_number,email,date_born, id_user])
+        client.release()
+        return resp_db
+
     }
 })
