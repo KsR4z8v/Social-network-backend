406930c711185c06da43ac2e1574ae9d2ae9eb5b
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-11-01 12:58:55 -0500

    refactoring:

--- src/handlersResponses/responseTemplates.js
+++ src/handlersResponses/responseTemplates.js
@@ -3,7 +3,7 @@ export default ({
     accountDeactivated: (message) => ({ status: 'ACCOUNT_DEACTIVATED', message: message ?? 'la cuenta se encuentra desactivada' }),
     incorrectCodeVerified: (message) => ({ status: 'CODE_INCORRECT', message: message ?? 'Código de verificación incorrecto. Por favor verifique su correo e intentelo de nuevo.' }),
     dataAlreadyExist: (message) => ({ status: 'USER_ALREADY_EXIST', message: message ?? 'El correo o el username ya esta en uso' }),
-    passwordIncorrect: (message) => ({ status: 'PASSWORD_INCORRECT', message: message ?? 'the password is incorrect' }),
+    passwordIncorrect: (message) => ({ status: 'PASSWORD_INCORRECT', message: message ?? 'La contraseña es incorrecta' }),
     internalError: (message) => ({ status: 'INTERNAL_SERVER_ERROR', message: message ?? 'Error interno del servidor' }),
     invalidBodyKeys: (message) => ({ status: 'INVALID_BODY_KEYS', message: message ?? 'La estructura del body es incorrecta, faltan keys o son incorrectas' }),
     invalidDateFormat: (message) => ({ status: 'INVALID_DATE_FORMAT', message: message ?? 'El formato de fecha es incorrecto' }),
