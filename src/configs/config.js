a1df0f92d75ceeb27642c3d7de76a9dfb3e152de
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-11-04 19:20:14 -0500

    chore:

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,14 +3,12 @@ dotenv.config()
 
 export default {
     config_cors: {
-        origin: ["http://localhost:3000", "*"],
+        origin: ["https://nt4mmhp7-3000.use2.devtunnels.ms", 'http://localhost:3000'],
         credentials: true,
     },
     IMAGE_KIT_CONFIG: {
         publicKey: process.env.PUBLIC_KEY_IMAGEKIT,
         privateKey: process.env.PRIVATE_KEY_IMAGEKIT,
         urlEndpoint: process.env.URL_ENDPOINT_IMAGEKIT,
-        images_folder_dest: process.env.IMAGES_FOLDER_DEST,
-        avatars_folder_dest: process.env.AVATARS_FOLDER_DEST,
     }
 }
