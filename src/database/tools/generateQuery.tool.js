0023d44f330798ef1671574d8f858cec38d32b8c
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-11-07 15:06:19 -0500

    feat: select generator and improved logic

--- src/database/tools/generateQuery.tool.js
+++ src/database/tools/generateQuery.tool.js
@@ -1,10 +1,10 @@
-export const generateQuery = (id_user, data) => {
-    const generate_payload = (type) => {
+export const generateQuery = (table) => {
+    const generate_payload = (type, data) => {
         const keys = [];
         const values = []
         let index = 1
         for (const i in data) {
-            if (type === 'to_update') {
+            if (type === 'to_update' || type === 'to_select') {
                 keys.push(`${i} = ${'$' + index}`)
             }
             if (type === 'to_insert') {
@@ -13,23 +13,25 @@ export const generateQuery = (id_user, data) => {
             values.push(data[i])
             index++
         }
-        values.push(id_user)
         return { keys, values }
     }
-
     return ({
-        update: (table) => {
-            const { keys, values } = generate_payload('to_update')
+        update: (id_user, data) => {
+            const { keys, values } = generate_payload('to_update', data)
+            values.push(id_user)
             console.log(keys, values);
             const query = `UPDATE ${table} SET  ${keys.join(', ')} WHERE id_user =${'$' + values.length} RETURNING id_user`
             return { query, values }
         },
-        insert: (table) => {
-            const { keys, values } = generate_payload('to_insert')
-            values.pop()
+        insert: (data) => {
+            const { keys, values } = generate_payload('to_insert', data)
             const query = `INSERT INTO ${table} (${Object.keys(data).join(', ')} ) VALUES (${keys.join(', ')}) RETURNING id_user`
             return { query, values }
+        },
+        select: (filters, selectors) => {
+            const { keys, values } = generate_payload('to_select', filters)
+            const query = `SELECT ${selectors.join(',')} FROM ${table} WHERE ${keys.join(' or ')}`
+            return { query, values }
         }
-
     })
 }
