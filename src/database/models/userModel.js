2deb58ebe381a0e9b54c0928d9d139b13ff8dd37
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-11-07 15:06:19 -0500

    feat: select generator and improved logic

--- src/database/models/userModel.js
+++ src/database/models/userModel.js
@@ -16,21 +16,32 @@ export default (pool) => {
             return user_found.rows[0]
         },
         getInfoUserById: async (id_user,) => {
-            const user_found = await source('SELECT fullname,username,phone_number,email,url_avatar,date_born,verify_code,password,user_bio from users where id_user = $1', [id_user])
+            const { query, values } = generateQuery('users').select({ id_user }, ["fullname", "username", "phone_number", "email", "url_avatar", "date_born", "verify_code", "password", "user_bio"])
+            // const user_found = await source('SELECT fullname,username,phone_number,email,url_avatar,date_born,verify_code,password,user_bio from users where id_user = $1', [id_user])
+            console.log('QUERY GENERADA', query)
+            const user_found = await source(query, values)
             return user_found.rows[0]
         },
         verifyIfExistUser: async (username, email) => {
-            const user_found = await source('SELECT username,email from users where email = $1 or username= $2', [email, username])
+            const { query, values } = generateQuery('users').select({ username, email }, ['username', 'email'])
+            console.log('QUERY GENERADA', query)
+            const user_found = await source(query, values)
+            return user_found.rows[0]
+        },
+        getUser: async (filters, selectors) => {
+            const { query, values } = generateQuery('users').select(filters, selectors)
+            console.log('QUERY GENERADA', query, values);
+            const user_found = await source(query, values)
             return user_found.rows[0]
         },
         insertUser: async (data) => {
-            const { query, values } = generateQuery(undefined, data).insert('users')
+            const { query, values } = generateQuery('users').insert(data)
             console.log(query, values);
             const user_insert = await source(query, values)
             return user_insert.rows[0]
         },
         updateDataUserById: async (id_user, data) => {
-            const { query, values } = generateQuery(id_user, data).update('users')
+            const { query, values } = generateQuery('users').update(id_user, data)
             const resp_db = await source(query, values)
             console.log('query realizada', query)
             return resp_db.rows[0]
