02ce2a2947da5a55ab03afdf05becb95c9b54521
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-11-10 00:21:22 -0500

    refactor

--- src/controllers/user/getInfoUser.controller.js
+++ src/controllers/user/getInfoUser.controller.js
@@ -1,23 +1,39 @@
 
 import responseTemplate from '../../handlersResponses/responseTemplates.js';
 import models from '../../database/models/index.js';
-const { internalError, userNotFound } = responseTemplate
+import { query } from 'express';
+const { internalError, accountDeactivated } = responseTemplate
 const getInfoUserController = async (req, resp) => {
     try {
         const id_user = req.id_user
-        const resp_db = await models.userModels.getUser({ id_user }, [
-            "fullname",
-            "username",
-            "phone_number",
-            "email",
-            "url_avatar",
-            "date_born",
-            "id_user",
-            "user_bio"])
 
+        let resp_db;
+        let message_resp;
+
+        if (req.query.view_foreign) {
+            resp_db = await models.userModels.getUser({ id_user: parseInt(req.query.view_foreign) }, ['fullname', 'username', 'url_avatar', 'user_bio', 'state_account'])
+            message_resp = accountDeactivated('Esta cuenta se encuentra desactivada')
+        }
+        if (!resp_db) {
+            resp_db = await models.userModels.getUser({ id_user }, [
+                "fullname",
+                "username",
+                "phone_number",
+                "email",
+                "url_avatar",
+                "date_born",
+                "id_user",
+                "user_bio",
+                "state_account"])
+            message_resp = accountDeactivated('Tu cuenta ha sido desactivada')
+        }
         if (!resp_db) {
             return resp.status(404).json(userNotFound())
         }
+        if (!resp_db.state_account) {
+            return resp.status(403).json(message_resp)
+        }
+
         resp_db.url_avatar = resp_db.url_avatar?.split('*key:*').shift()
         resp.status(200).json(resp_db)
     } catch (error) {
