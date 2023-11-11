c4860c1803031cfd7328d44867a99e10d16c7850
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-11-11 13:34:10 -0500

    refactor:

--- src/controllers/user/data.update.controller.js
+++ src/controllers/user/data.update.controller.js
@@ -5,7 +5,14 @@ const { internalError, userNotFound } = responseTemplate
 const dataUpdateController = async (req, resp) => {
     try {
         const id_user = req.id_user
-        const resp_db = await models.userModels.updateDataUserById(id_user, { ...req.body })
+
+        const FUNCTIONS_TYPE = {
+            config: models.userModels.updateSettingsUserById,
+        }
+
+
+        const action_function = FUNCTIONS_TYPE[req.query?.data] || models.userModels.updateDataUserById
+        const resp_db = await action_function(id_user, { ...req.body })
         if (!resp_db) {
             return resp.status(404).json(userNotFound())
         }
