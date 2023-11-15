c76ae997aa661290f0d7c41ad7927639b74e0d6a
mauricio pacichana <mauricioxp018@gmail.com>
2023-11-14 19:04:40 -0500

    add likes y comentarios

--- src/routes/v1/comment.routes.js
+++ src/routes/v1/comment.routes.js
@@ -3,13 +3,14 @@ import controllers from '../../controllers/index.js'
 import verify_token from '../../middlewares/verify_token.middleware.js'
 
 const post_routes = Router()
-post_routes.get('/id:post', verify_token , controllers.getc)
+
+post_routes.get('/id:post', verify_token , controllers.getComments)
 post_routes.post('/createComment/:id_post', verify_token, controllers.createComment);
 post_routes.put('/editComment/:id_comment', verify_token, controllers.editComment);
 post_routes.put('/deleteComment/:id_comment', verify_token, controllers.deleteComment);
 
 
-post_routes.put('/like/:id_post', verify_token, controllers.likePostController );
+
 
 
 export default post_routes
