a5dd6b1b80565464f61e6756627a1ee0d1150716
dvz <79332655+KsR4z8v@users.noreply.github.com>
2023-11-19 11:28:34 -0500

    refactor: modify post

--- src/routes/v1/post.routes.js
+++ src/routes/v1/post.routes.js
@@ -7,9 +7,7 @@ const post_routes = Router()
 // controllers.createPostController
 post_routes.post('/createPost', verify_token, middleware_CreatePost, controllers.createPostController)
 post_routes.get('/getPosts', verify_token, controllers.getPostsController)
-
-
-post_routes.put('/like/:id_post', verify_token, controllers.likePostController );
-
+post_routes.put('/like/:id_post', verify_token, controllers.likePostController);
+post_routes.post('/modify_post/:id_post', verify_token, controllers.modifyPostController)
 
 export default post_routes
