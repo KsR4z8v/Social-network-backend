24217167f6de8798bce1715330121ba90458f6ef
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-12-02 08:04:28 -0500

    refactor:

--- src/controllers/posts/reportPost.controller.js
+++ src/controllers/posts/reportPost.controller.js
@@ -6,17 +6,18 @@ const reportPostController = async (req, res) => {
     try {
         const id_post = req.params.id_post;
         const id_user = req.id_user;
-        const reason = req.body.reason;
+        const { reason, type_report } = req.body;
 
-        const post = await models.postModels.getPostById(id_post);
+        const post = await models.postModels.getPostById(parseInt(id_post));
 
-        if (!post || post.state_post === false) {
+        if (!post || post.post_status === false) {
             return res.status(404).json({ message: 'la publicacion no existe' });
         } else {
-            const report = await models.postModels.insertReport({ id_post: id_post, id_user: id_user, reason: reason, date: new Date() });
+            const report = await models.postModels.insertReport({ id_post, id_user, reason, type_report, date_report: new Date() });
             return res.status(200).json({ message: 'Ok' })
         }
-    } catch (err) {
+    } catch (e) {
+        console.log(e);
         return res.status(500).json(internalError());
     }
 }
