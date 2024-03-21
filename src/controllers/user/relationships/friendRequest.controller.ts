b3a643057dc8a15847d16bf987075e44794a8612
dvz <79332655+KsR4z8v@users.noreply.github.com>
2024-03-21 05:40:52 -0500

    fix: self request

--- src/controllers/user/relationships/friendRequest.controller.ts
+++ src/controllers/user/relationships/friendRequest.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import MongoUserRepository from "../../../database/repositories/MongoUserRepository";
 import ErrorHandler from "../../../helpers/ErrorHandler";
+import IncorrectDataRequest from "../../../exceptions/IncorrectDataRequest";
 
 export default class FriendRequestController {
   constructor(
@@ -10,6 +11,11 @@ export default class FriendRequestController {
   async run(req: Request, res: Response) {
     const { id_user, to_user } = req.params;
     try {
+      if (id_user === to_user) {
+        throw new IncorrectDataRequest(
+          "No te puedes enviar solicitud a ti mism"
+        );
+      }
       const resp_db = await this.userRepository.sendFriendRequest(
         id_user,
         to_user
