d78f9a89ea6059870bc9cb39c43690aed06c627d
dvz <79332655+KsR4z8v@users.noreply.github.com>
2024-05-10 18:38:37 -0500

    fix(service): redis connection and refactoring

--- src/database/redis/VerifyCodeRedisService.ts
+++ src/database/redis/VerifyCodeRedisService.ts
@@ -1,32 +1,39 @@
-import { RedisClientType } from "redis";
+import { type RedisClientType } from "redis";
+
 class VerifyCodeRedisService {
   private static instance: VerifyCodeRedisService;
-  private client: RedisClientType;
-  constructor(client: RedisClientType) {
-    this.client = client;
-    if (VerifyCodeRedisService.instance) {
+
+  constructor(readonly client: RedisClientType) {
+    if (VerifyCodeRedisService.instance instanceof VerifyCodeRedisService) {
       return VerifyCodeRedisService.instance;
     }
     VerifyCodeRedisService.instance = this;
     return this;
   }
-  static getInstance() {
+
+  static getInstance(): VerifyCodeRedisService {
     return VerifyCodeRedisService.instance;
   }
-  async setVerificationCode(id_user: string, verification_code: string) {
-    await this.client.connect();
-    await this.client.set(id_user, verification_code, { EX: 60 * 10 });
-    await this.client.disconnect();
-  }
-  async getVerificationCode(id_user: string) {
+
+  async setVerificationCode(
+    idUser: string,
+    verificationCode: string,
+  ): Promise<void> {
     const connection = await this.client.connect();
-    const data = await connection.get(id_user);
+    await connection.set(idUser, verificationCode, { EX: 60 * 10 });
+    await connection.disconnect();
+  }
+
+  async getVerificationCode(idUser: string): Promise<string | null> {
+    const connection = await this.client.connect();
+    const data = await connection.get(idUser);
     await connection.disconnect();
     return data;
   }
-  async deleteVerificationCode(id_user: string) {
+
+  async deleteVerificationCode(idUser: string): Promise<void> {
     const connection = await this.client.connect();
-    await connection.del(id_user);
+    await connection.del(idUser);
     await connection.disconnect();
   }
 }
