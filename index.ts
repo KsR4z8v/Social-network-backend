61a7edf662e90a1a7471f00e6a5c95bf285ed898
Jose David Suarez Cardona <josedavid4227@gmail.com>
2024-05-18 16:17:53 -0500

    chore: new dependencies and new configs

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 import app from "./src/app";
 import dotenv from "dotenv";
-import { clientRedis } from "./src/configs/redis_connection";
+import redisClient from "./src/configs/ConnectionRedis";
 import VerifyCodeRedisService from "./src/database/redis/VerifyCodeRedisService";
 import ApiGoogleEmailService from "./src/services/sendEmailGoogle.service";
-dotenv.config();
+dotenv.config({ path: ".develop.env" });
 
-//dataBase redis
-new VerifyCodeRedisService(clientRedis);
+const initServices = (): void => {
+  // dataBase redis
+  void new VerifyCodeRedisService(redisClient);
 
-new ApiGoogleEmailService(
-  process.env.ID_CLIENT,
-  process.env.SECRET_CLIENT,
-  process.env.ACCESS_TOKEN_GOOGLE
-);
+  void new ApiGoogleEmailService(
+    process.env.ID_CLIENT ?? " ",
+    process.env.SECRET_CLIENT ?? " ",
+    process.env.REFRESH_TOKEN_GOOGLE ?? " ",
+  );
+};
+
+initServices();
 
 const PORT = process.env.PORT ?? 8000;
 
-//app
+// app
 app.listen(PORT, () => {
   console.log(`Server listen on port: ${PORT}`);
 });
---
+++ src/configs/ConnectionRedis.ts
@@ -0,0 +1,15 @@
+import { type RedisClientType, createClient } from "redis";
+
+const redisClient: RedisClientType = createClient({
+  url: process.env.URI_REDIS,
+});
+
+redisClient.on("error", (error) => {
+  console.error(`Redis client error:`, error);
+});
+
+redisClient.on("connect", () => {
+  console.error(`Redis client connect`);
+});
+
+export default redisClient;
